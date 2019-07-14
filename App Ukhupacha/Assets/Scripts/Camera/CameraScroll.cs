b82ba9e2783fd242020e8d45882d2db65e0c7d14
miblux27 <44678134+miblux27@users.noreply.github.com>
2019-07-14 17:47:56 +0200

    Camera Scroll

---
+++ App Ukhupacha/Assets/Scripts/Camera/CameraScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScroll : MonoBehaviour
+{
+    public Scrollbar scrollbar;
+    public Vector3 top;
+    public Vector3 bottom;
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(top, bottom, scrollbar.value);
+    }
+}
